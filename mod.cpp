6dd0d230aeb15b21495dd12cdb78df15abcb49ab
Connor <15099385+ConnorAU@users.noreply.github.com>
2025-04-28 13:19:11 +1000

    Add mod.cpp Adding to git for use in remote build workflow

---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name="UserInputMenus";
+overview="Multi-purpose user input displays for use in your missions and mods";
+tooltipOwned="UserInputMenus";
+action="https://github.com/ConnorAU/A3UserInputMenus";
+
+hideName=0;
+hidePicture=0;
